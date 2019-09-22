5bfd3d745a15a1aced5831f6582489831016e928
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-09-22 12:28:45 +0200

    Check code style in linting session

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,8 @@ locations = "src", "tests", "noxfile.py"
 @nox.session(python="3.7")
 def lint(session):
     """Lint using flake8."""
-    session.install("flake8", "flake8-bugbear", "flake8-import-order")
+    session.install("flake8", "flake8-bugbear", "flake8-import-order", "black")
+    session.run("black", "--check", *locations)
     session.run("flake8", *locations)
 
 
